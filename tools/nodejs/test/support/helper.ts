8b52f0948f5efd37b556ec828f74a0f590a854a3
Elliana May <me@mause.me>
2022-07-15 07:47:54 +0000

    chore: port some basic test support code

--- tools/nodejs/test/support/helper.ts
+++ tools/nodejs/test/support/helper.ts
@@ -2,33 +2,37 @@ var assert = require('assert');
 var fs = require('fs');
 var pathExists = require('fs').existsSync || require('path').existsSync;
 
-exports.deleteFile = function(name) {
+function isFileDNE(err: any) {
+    return err.code === 'ENOENT' && err.syscall === 'unlink';
+}
+
+export function deleteFile(name: string) {
     try {
         fs.unlinkSync(name);
     } catch(err) {
-        if (err.errno !== process.ENOENT && err.code !== 'ENOENT' && err.syscall !== 'unlink') {
+        if (!isFileDNE(err)) {
             throw err;
         }
     }
 };
 
-exports.ensureExists = function(name,cb) {
+export function ensureExists(name:string, cb: unknown) {
     if (!pathExists(name)) {
         fs.mkdirSync(name);
     };
 }
 
-assert.fileDoesNotExist = function(name) {
+assert.fileDoesNotExist = function(name: string) {
     try {
         fs.statSync(name);
     } catch(err) {
-        if (err.errno !== process.ENOENT && err.code !== 'ENOENT' && err.syscall !== 'unlink') {
+        if (!isFileDNE(err)) {
             throw err;
         }
     }
 };
 
-assert.fileExists = function(name) {
+assert.fileExists = function(name: string) {
     try {
         fs.statSync(name);
     } catch(err) {
