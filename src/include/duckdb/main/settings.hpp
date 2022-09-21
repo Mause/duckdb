8a69d4c06cee36df5c1d16fa28746518680384a7
Elliana May <me@mause.me>
2022-09-21 23:42:07 +0800

    chore: amend doc for http_proxy

--- src/include/duckdb/main/settings.hpp
+++ src/include/duckdb/main/settings.hpp
@@ -555,7 +555,7 @@ struct FlushAllocatorSetting {
 struct HttpProxySetting {
 	static constexpr const char *Name = "http_proxy";
 	static constexpr const char *Description =
-	    "The HTTP CONNECT proxy used to download extensions"; // TODO: add proxy support for s3/httpfs
+	    "The HTTP CONNECT proxy used to download extensions, and make http calls in the httpfs extension";
 	static constexpr const LogicalTypeId InputType = LogicalTypeId::VARCHAR;
 	static void SetGlobal(DatabaseInstance *db, DBConfig &config, const Value &parameter);
 	static Value GetSetting(ClientContext &context);
