aeebaef7d0aa31c77ca0eef081de56bb792b1b9e
Elliana May <me@mause.me>
2022-09-22 22:38:06 +0800

    chore: format

--- src/main/config.cpp
+++ src/main/config.cpp
@@ -389,14 +389,12 @@ bool DBConfig::operator!=(const DBConfig &other) {
 }
 
 std::string ProxyUri::ToString() const {
-	std::map<uri::component, string> components = {
-	    {uri::component::Scheme, "http"},
-	    {uri::component::Username, username},
-	    {uri::component::Password, password},
-	    {uri::component::Host, host},
-	    {uri::component::Port, std::to_string(port)},
-	    {uri::component::Path, ""}
-	};
+	std::map<uri::component, string> components = {{uri::component::Scheme, "http"},
+	                                               {uri::component::Username, username},
+	                                               {uri::component::Password, password},
+	                                               {uri::component::Host, host},
+	                                               {uri::component::Port, std::to_string(port)},
+	                                               {uri::component::Path, ""}};
 
 	return uri(components, uri::scheme_category::Hierarchical, false).to_string();
 }
