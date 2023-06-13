347d1f124913e6dc5737ea1fd925676451fc8183
Elliana May <me@mause.me>
2023-06-13 16:46:52 +0800

    load more data from table

--- tools/jdbc/app/src/main/java/org/duckdb/duckdb_jdbc/Extension.java
+++ tools/jdbc/app/src/main/java/org/duckdb/duckdb_jdbc/Extension.java
@@ -6,9 +6,17 @@ import java.sql.SQLException;
 class Extension {
 	String name;
 	boolean installed;
+	boolean loaded;
+	String install_path;
+	String description;
+	String[] aliases;
 
 	public Extension(ResultSet resultSet) throws SQLException {
 		name = resultSet.getString("extension_name");
 		installed = resultSet.getBoolean("installed");
+		loaded = resultSet.getBoolean("loaded");
+		install_path = resultSet.getString("install_path");
+        description = resultSet.getString("description");
+        aliases = (String[]) resultSet.getArray("aliases").getArray();
 	}
 }
