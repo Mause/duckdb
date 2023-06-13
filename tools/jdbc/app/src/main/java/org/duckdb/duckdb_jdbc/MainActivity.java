7c8abfcd66b8336dbda0fdce0445840af81e1db4
Elliana May <me@mause.me>
2023-06-14 01:52:40 +0800

    let the user input the query

--- tools/jdbc/app/src/main/java/org/duckdb/duckdb_jdbc/MainActivity.java
+++ tools/jdbc/app/src/main/java/org/duckdb/duckdb_jdbc/MainActivity.java
@@ -1,6 +1,10 @@
 package org.duckdb.duckdb_jdbc;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.GridLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -12,24 +16,32 @@ import org.duckdb.DuckDBDriver;
 import org.duckdb.duckdb_jdbc.databinding.ActivityMainBinding;
 
 import java.sql.Array;
+import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Objects;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
+	private final ExecutorService pool = Executors.newCachedThreadPool();
+	private Connection connect;
+	private ActivityMainBinding binding;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		var layoutInflater = getLayoutInflater();
-		var binding = ActivityMainBinding.inflate(layoutInflater);
+		binding = ActivityMainBinding.inflate(layoutInflater);
+		binding.executeButton.setOnClickListener(this::onClick);
 		setContentView(binding.getRoot());
 
-		var results = binding.results;
-
 		var duckDBDriver = new DuckDBDriver();
-		try (var connect = duckDBDriver.connect("jdbc:duckdb:", null)) {
+		try {
+			connect = duckDBDriver.connect("jdbc:duckdb:", null);
+
 			try (var stmt = connect.prepareStatement("select concat('We are running DuckDB version ', version())");
 				 var resultSet = stmt.executeQuery()) {
 				if (resultSet.next()) {
@@ -37,28 +49,59 @@ public class MainActivity extends AppCompatActivity {
 				}
 			}
 
-			try (var stmt = connect.prepareStatement("select * from duckdb_extensions()");
-				 var resultSet = stmt.executeQuery()) {
-
-				loadIntoGridView(results, resultSet);
-			}
+			setGridData(getString(R.string.sql_input_helper_text));
 		} catch (Throwable t) {
-			t.printStackTrace();
-			Toast.makeText(this, t.toString(), Toast.LENGTH_LONG).show();
+			handleException(t);
 		}
+	}
+
+	private void onClick(View view) {
+		EditText editText = binding.sqlInput.getEditText();
+		if (editText != null) {
+			pool.submit(() -> {
+				Looper.prepare();
+				setGridData(editText.getText().toString());
+			});
+		}
+	}
+
+	private void setGridData(String sql) {
+		var results = binding.results;
+		try (var stmt = connect.prepareStatement(sql);
+			 var resultSet = stmt.executeQuery()) {
+
+			loadIntoGridView(results, resultSet);
+		} catch (Throwable t) {
+			handleException(t);
+		}
+	}
+
+	private void handleException(Throwable t) {
+		while (t.getCause() instanceof SQLException) {
+			t = t.getCause();
+		}
+		t.printStackTrace();
+		var message = t.getMessage();
+		if (message != null && message.contains(": ")) {
+			String[] parts = message.split(": ", 2);
+			message = parts[1];
+		}
+		Toast.makeText(this, message, Toast.LENGTH_LONG).show();
 	}
 
 	private void loadIntoGridView(GridLayout results, ResultSet resultSet) throws SQLException {
 		var metaData = resultSet.getMetaData();
-		results.setColumnCount(metaData.getColumnCount());
+		int columnCount = metaData.getColumnCount();
+
+		var views = new ArrayList<String>();
 
 		int rows = 1;
-		for (int i = 0; i< metaData.getColumnCount(); i++) {
-			results.addView(make(metaData.getColumnName(i+1)));
+		for (int i = 0; i < columnCount; i++) {
+			views.add(metaData.getColumnName(i + 1));
 		}
 
 		while (resultSet.next()) {
-			for (int i = 0; i< metaData.getColumnCount(); i++) {
+			for (int i = 0; i < columnCount; i++) {
 				Object object = resultSet.getObject(i + 1);
 				String string;
 				if (object instanceof Array) {
@@ -66,11 +109,18 @@ public class MainActivity extends AppCompatActivity {
 				} else {
 					string = Objects.toString(object);
 				}
-				results.addView(make(string));
+				views.add(string);
 			}
 			rows++;
 		}
-		results.setRowCount(rows);
+
+		int finalRows = rows;
+		new Handler(Looper.getMainLooper()).post(() -> {
+			results.removeAllViews();
+			results.setColumnCount(columnCount);
+			views.forEach(s -> results.addView(make(s)));
+			results.setRowCount(finalRows);
+		});
 	}
 
 	@NonNull
