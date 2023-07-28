b224919a19c9a20114a652c74d23c1f9c1e990f3
Elliana May <me@mause.me>
2023-07-29 00:45:46 +0800

    error if we can't find any work

--- tools/pythonpkg/create_extension_package.py
+++ tools/pythonpkg/create_extension_package.py
@@ -44,9 +44,71 @@ def auditwheel_repair(target: PathLike) -> None:
     ags.func(ags, p)
 
 
+def process_extension(extension_name: str) -> None:
+    source = (Path(args.source_folder) / extension_name / f'{extension_name}.duckdb_extension').resolve()
+    if not source.exists():
+        print(source, 'is missing')
+        return False
+
+    target = base / extension_name
+    module_name = f'duckdb_extension_{extension_name}'
+    module = target / module_name
+    module.mkdir(parents=True, exist_ok=True)
+
+    with (target / 'pyproject.toml').open('w') as fh:
+        toml.dump(pyproject(extension_name), fh)
+
+    with (target / 'setup.py').open('w') as fh:
+        fh.write(
+            dedent(
+                f'''\
+        from setuptools import setup
+
+        setup()
+        '''
+            )
+        )
+
+    copyfile(source, module / source.name)
+    with (module / '__init__.py').open('w') as fh:
+        fh.write(
+            dedent(
+                f'''\
+        import importlib.resources as pkg_resources
+
+        __all__ = ['extension']
+
+        def extension():
+            files = list(pkg_resources.files(__name__).iterdir())
+            result = next((f for f in files if f.name.endswith('.duckdb_extension')), None)
+            assert result, files
+            return pkg_resources.as_file(result)
+
+        '''
+            )
+        )
+
+    (module / '__main__.py').write_text(
+        dedent(
+            '''
+        from . import extension
+        print(extension())
+        '''
+        )
+    )
+
+    print('templated', extension_name)
+
+    if args.build:
+        build([str(target), '--wheel'])
+        auditwheel_repair(list((target / 'dist').glob('*.whl'))[0])
+
+    return True
+
+
 def main():
     rmtree(base, ignore_errors=True)
-    for extension_name in [
+    extension_names = [
         'autocomplete',
         'excel',
         'fts',
@@ -59,64 +121,10 @@ def main():
         'tpcds',
         'tpch',
         'visualizer',
-    ]:
-        source = (Path(args.source_folder) / extension_name / f'{extension_name}.duckdb_extension').resolve()
-        if not source.exists():
-            print(source, 'is missing')
-            continue
+    ]
 
-        target = base / extension_name
-        module_name = f'duckdb_extension_{extension_name}'
-        module = target / module_name
-        module.mkdir(parents=True, exist_ok=True)
-
-        with (target / 'pyproject.toml').open('w') as fh:
-            toml.dump(pyproject(extension_name), fh)
-
-        with (target / 'setup.py').open('w') as fh:
-            fh.write(
-                dedent(
-                    f'''\
-            from setuptools import setup
-
-            setup()
-            '''
-                )
-            )
-
-        copyfile(source, module / source.name)
-        with (module / '__init__.py').open('w') as fh:
-            fh.write(
-                dedent(
-                    f'''\
-            import importlib.resources as pkg_resources
-
-            __all__ = ['extension']
-
-            def extension():
-                files = list(pkg_resources.files(__name__).iterdir())
-                result = next((f for f in files if f.name.endswith('.duckdb_extension')), None)
-                assert result, files
-                return pkg_resources.as_file(result)
-
-            '''
-                )
-            )
-
-        (module / '__main__.py').write_text(
-            dedent(
-                '''
-            from . import extension
-            print(extension())
-            '''
-            )
-        )
-
-        print('templated', extension_name)
-
-        if args.build:
-            build([str(target), '--wheel'])
-            auditwheel_repair(list((target / 'dist').glob('*.whl'))[0])
+    if not any(process_extension(extension_name) for extension_name in extension_names):
+        parser.error("Couldn't process any extensions")
 
 
 if __name__ == '__main__':
