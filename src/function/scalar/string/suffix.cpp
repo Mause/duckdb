d862c37b6a04f901b98a0a3549f6b780e895888e
Elliana May <me@mause.me>
2023-08-31 11:54:12 +0800

    Add ends_with alias

--- src/function/scalar/string/suffix.cpp
+++ src/function/scalar/string/suffix.cpp
@@ -34,14 +34,13 @@ static bool SuffixFunction(const string_t &str, const string_t &suffix) {
 }
 
 ScalarFunction SuffixFun::GetFunction() {
-	return ScalarFunction("suffix",                                     // name of the function
-	                      {LogicalType::VARCHAR, LogicalType::VARCHAR}, // argument list
+	return ScalarFunction({LogicalType::VARCHAR, LogicalType::VARCHAR}, // argument list
 	                      LogicalType::BOOLEAN,                         // return type
 	                      ScalarFunction::BinaryFunction<string_t, string_t, bool, SuffixOperator>);
 }
 
 void SuffixFun::RegisterFunction(BuiltinFunctions &set) {
-	set.AddFunction(GetFunction());
+	set.AddFunction({"suffix", "ends_with"}, GetFunction());
 }
 
 } // namespace duckdb
